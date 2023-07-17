f2762e6b7b0f9d1d7d21a35589ecaca5a8192652
irfanrizqinr <80774682+irfanrizqinr@users.noreply.github.com>
2023-07-18 01:31:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Who's birthday today??");
+ var b=prompt("macaci, tanggal lahirnya berapa emang (format = dd-mm-yyyy)");
+  	if(a== "Annabelle Andjani" && b=="17-07-2005")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("waduh ada yang salah tuh coba lagi bel");
+  		window.location.href="index.html";
+  	}
+  	
